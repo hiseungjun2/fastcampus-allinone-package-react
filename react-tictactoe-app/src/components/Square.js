b70ffde7a570458e97320bdded10ddb7dcf8ce57
김승준 <hiseungjun2@gmail.com>
2024-09-05 20:03:00 +0900

    props 심화 강의

--- react-tictactoe-app/src/components/Square.js
+++ react-tictactoe-app/src/components/Square.js
@@ -2,18 +2,11 @@ import React from "react"
 import "./Square.css"
 
 export default class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value : null
-    }
-  }
-  
   render() {
     return (
       <button className="square"
-        onClick={() => this.setState({value : "x"})}>
-        {this.state.value}
+        onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     )
   }
