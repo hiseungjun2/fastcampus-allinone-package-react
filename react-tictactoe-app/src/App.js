42d437f153acf6deeea1b931bb44fc7fe5a96c69
김승준 <hiseungjun2@gmail.com>
2024-09-08 21:54:45 +0900

    함수형 컴포넌트 전환 및 history 컴포넌트 작성 등

--- react-tictactoe-app/src/App.js
+++ react-tictactoe-app/src/App.js
@@ -1,14 +1,76 @@
+import { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 
-function App() {
+const App = () => {
+
+  const [history, setHistory] = useState([{squares : Array(9).fill(null)}]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [7,8,9],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a,b,c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+
+  let status;
+  if (winner) {
+    status = `Winner : ${winner}`;
+  } else {
+    status = `Next player : ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice();
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setHistory([...history, { squares : newSquares }]);
+    setXIsNext(prev => !prev);
+  }
+
+  const moves = history.map((step, move) => {
+    const desc = move ? 
+    `Go to move # ${move}` :
+    `Go to game start`;
+
+    return (
+      <li key={move}>
+        <button>{desc}</button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
       <div className='game-board'>
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)}/>
       </div>
       <div className='game-info'>
-        game-info
+        <div className='status'>{status}</div>
+        <ol>
+          {moves}
+        </ol>
       </div>
     </div>
   );
