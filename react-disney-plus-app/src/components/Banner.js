3dc29a633bd71737c81aa80fae56cb14a8371245
김승준 <hiseungjun2@gmail.com>
2024-10-13 14:12:14 +0900

    nav 및 banner 진행중

--- react-disney-plus-app/src/components/Banner.js
+++ react-disney-plus-app/src/components/Banner.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from '../api/axios'
+import requests from '../api/request'
 
 const Banner = () => {
+
+  useEffect(() => {
+    fetchData();
+  
+    
+  }, [])
+
+  const fetchData = () => {
+    const response = axios.get(requests.fetchNowPlaying);
+    console.log(response);
+  }
+  
   return (
     <div>
-      
+      Banner 
     </div>
   )
 }
